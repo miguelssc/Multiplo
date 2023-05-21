d5c1766848524fbca7a6018d1c657b22be10edbb
miguel <miguelpositivo02@gmail.com>
2023-05-21 19:35:21 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+int divisor;
+Console.WriteLine("digite um numero");
+numero =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite outro numero");
+divisor =Convert.ToInt32(Console.ReadLine());
+
+int definiçao = numero % divisor;
+
+if(definiçao == 0){
+    Console.WriteLine($"{numero} e multiplo de {divisor}");
+}
+else{
+    Console.WriteLine($"{numero} nao e multiplo de {divisor}");
+}
+
